77f99bfaf1373034da3e83d67091f44c161c2c37
Shrey Patel <shreynpatel23@gmail.com>
2020-12-05 09:35:51 +0530

    add post view in front end (#16)
    Add Post view

--- src/components/forum/discussion/add-discussion/addDiscussion.tsx
+++ src/components/forum/discussion/add-discussion/addDiscussion.tsx
@@ -1,69 +1,191 @@
-import React, { useState, useRef, useEffect } from "react";
+// import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import Styles from "./addDiscussion.module.scss";
-import ReactQuill from "react-quill";
+import Button from "../../../../shared/button/button";
+import { primaryButtonStyle } from "../../../../shared/buttonStyles";
+import { useHistory } from "react-router-dom";
+import addNewPost from "../../../../api/addNewPost";
 function AddDiscussion() {
-  const quillRef = useRef(null);
-  const [bodyText, setBodyText] = useState();
-  const modules = {
-    toolbar: [
-      ["bold"], // toggled buttons
-      ["italic"], // toggled buttons
-      ["underline"], // toggled buttons
-      ["strike"], // toggled buttons
-      ["blockquote", "code-block"],
+  const history = useHistory();
+  const headingRef: any = useRef(null);
+  const bodyRef: any = useRef(null);
+  const [error, setError] = useState("");
+  console.log(error);
+  let formatType = "";
 
-      [{ header: 1 }, { header: 2 }], // custom button values
-      [{ list: "ordered" }, { list: "bullet" }],
-      [{ script: "sub" }, { script: "super" }], // superscript/subscript
-      [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-      [{ font: [] }],
-      [{ align: [] }],
-    ],
+  // use this function for creating a new post
+  const handleAddPost = async () => {
+    const headingTextRef = headingRef?.current;
+    const bodyTextRef = bodyRef?.current;
+    const headerText = headingTextRef.innerText;
+    const bodyText = bodyTextRef.innerText;
+    const headerHTML = headingTextRef.innerHTML;
+    let bodyHTML = bodyTextRef.innerHTML;
+    const tags = [];
+    const category = "Tech";
+    const comments = [];
+    const pinned = false;
+    const saved = false;
+    const liked = {
+      count: 0,
+      isLiked: false,
+    };
+    bodyHTML = JSON.stringify(bodyHTML);
+    console.log(bodyHTML);
+    const discussion: any = await addNewPost({
+      headerText,
+      bodyText,
+      headerHTML,
+      bodyHTML,
+      tags,
+      category,
+      comments,
+      pinned,
+      saved,
+      liked,
+    }).catch((err) => {
+      console.log(err);
+    });
+    history.push(`/forum/discussion/${discussion.data._id}`, {
+      discussion: discussion.data,
+    });
   };
 
-  const formats = [
-    "header",
-    "font",
-    "size",
-    "bold",
-    "italic",
-    "underline",
-    "align",
-    "strike",
-    "script",
-    "blockquote",
-    "background",
-    "list",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-    "color",
-    "code-block",
-  ];
-  const handleChange = (range) => {
-    setBodyText(range);
-  };
-  useEffect(() => {
-    if (quillRef.current) {
-      console.log(quillRef?.current.getEditor());
+  // use this function to create a list ordered as well as unordered.
+  const addListStyle = (value: string) => {
+    let listWrapper: any;
+    let list: any;
+    if (value === "unorder") {
+      listWrapper = document.createElement(`ul`);
+    } else if (value === "order") {
+      listWrapper = document.createElement(`ol`);
     }
-  }, [quillRef]);
+    list = document.createElement(`li`);
+    listWrapper.appendChild(list);
+    bodyRef?.current.appendChild(listWrapper);
+  };
+
+  const handleTextFormat = (value: string) => {
+    if (value === "bold") {
+      formatType = "bold";
+    } else if (value === "italic") {
+      formatType = "italic";
+    } else if (value === "underline") {
+      formatType = "underline";
+    } else if (value === "strikeThrough") {
+      formatType = "strikeThrough";
+    }
+    let selObj: any = window.getSelection();
+    if (selObj.rangeCount > 0) {
+      selObj.getRangeAt(0);
+      window.document.execCommand(formatType);
+    } else {
+      console.log("empty string");
+    }
+  };
+
+  const uploadFile = (file: any) => {
+    // added check for the file type
+    if (
+      file.name.includes("jpeg") ||
+      file.name.includes("jpg") ||
+      file.name.includes("png")
+    ) {
+      // added basic check for the comparing the file size. if it is less than 2MB.
+      if (file.size < 2000000) {
+        const reader = new FileReader();
+        // converting the image name into base64 url
+        reader.readAsDataURL(file);
+        reader.onload = (event: any) => {
+          let image = document.createElement(`img`);
+          image.src = event.target.result;
+          image.alt = "uploaded_image";
+          image.style.padding = "20px";
+          image.style.width = "50%";
+          image.style.height = "50%";
+          image.height = 200;
+          bodyRef?.current.appendChild(image);
+          setError("");
+        };
+      } else {
+        setError("file should be less than 2 MB");
+        return;
+      }
+    } else {
+      // throw error that we only support png and jpeg files.
+      setError("Image should be in png or jpeg format");
+      return;
+    }
+  };
+
   return (
-    <div className={Styles.background}>
-      <div className={`${Styles.addDiscussionCard} p-5`}>
-        <ReactQuill
-          value={bodyText}
-          onChange={(range) => handleChange(range)}
-          placeholder="Enter text here"
-          ref={quillRef}
-          className={Styles.editor}
-          modules={modules}
-          formats={formats}
-        />
+    <div className={`${Styles.background} px-4 py-5`}>
+      <div style={{ height: "10%" }} className="d-flex align-items-center">
+        <div
+          className={Styles.header}
+          placeholder="Type something Heading text"
+          contentEditable="true"
+          ref={headingRef}
+        ></div>
+      </div>
+      <div style={{ height: "5%" }}>
+        <div
+          style={{ display: "flex", padding: "10px 0", alignItems: "center" }}
+        >
+          <button onClick={() => handleTextFormat("bold")}>bold</button>
+          <button onClick={() => handleTextFormat("italic")}>Italic</button>
+          <button onClick={() => handleTextFormat("underline")}>
+            UnderLine
+          </button>
+          <button onClick={() => handleTextFormat("strikeThrough")}>
+            strike through
+          </button>
+          <button onClick={() => addListStyle("unorder")}>UL</button>
+          <button onClick={() => addListStyle("order")}>LI</button>
+          <button>
+            <label htmlFor="uploadImage">
+              upload image
+              <input
+                id="uploadImage"
+                accept="image/png, image/jpeg"
+                type="file"
+                onChange={(e: any) => {
+                  uploadFile(e.target.files[0]);
+                }}
+                style={{ visibility: "hidden", display: "none" }}
+              />
+            </label>
+          </button>
+        </div>
+      </div>
+      <div style={{ height: "80%", overflow: "auto", padding: "20px" }}>
+        <p
+          className={Styles.bodyText}
+          placeholder="Type something body text"
+          contentEditable="true"
+          ref={bodyRef}
+          onDrop={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            uploadFile(e.dataTransfer.files[0]);
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+          }}
+          onDragLeave={(e) => {
+            e.preventDefault();
+          }}
+        ></p>
+      </div>
+      <div
+        style={{ height: "5%" }}
+        className="d-flex align-items-center justify-content-end"
+      >
+        <div style={{ width: "150px" }}>
+          <Button style={primaryButtonStyle} onClick={() => handleAddPost()}>
+            Add Post
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/shared/top-nav/topNav.tsx
+++ src/shared/top-nav/topNav.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Styles from "./topNav.module.scss";
-function TopNav() {
-  const user = JSON.parse(localStorage.getItem("@user"));
+function TopNav({ data }: any) {
   return (
     <div
       className={`py-3 px-2 d-flex align-items-center sticky-top ${Styles.background}`}
       style={{ zIndex: 2 }}
     >
       <div className="px-2">
-        <p className={`mb-0 ${Styles.communityName}`}>Comunity Name</p>
+        <p className={`mb-0 ${Styles.communityName}`}>{data.forumName}</p>
       </div>
       <div className="px-3 ml-auto">notifi</div>
       <img
-        src={user.imageUrl}
+        src={data.user.imageUrl}
         alt="url"
         className={`px-2 ${Styles.profilePicture}`}
       />
